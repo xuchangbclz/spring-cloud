286bbc4385640570781b769caab478e0bcf1c3eb
xc <792453741@qq.com>
2018-03-22 11:51:40 +0800

    make item

---
+++ spring-cloud-config/src/main/java/com/aisino/SpringCloudConfigApplication.java
@@ -0,0 +1,12 @@
+package com.aisino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigApplication.class, args);
+	}
+}
